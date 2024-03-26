900ac5ff0f1e591b01b2e91572dfd4a6762fcae5
Ali Reza <alireza.alemi@tdk.com>
2024-03-26 10:57:08 -0600

    reorganized project files.

---
+++ TBTNavigation/ApplicationFlow/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  TBTNavigation
+//
+//  Created by Ali Reza Alemi on 3/26/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
